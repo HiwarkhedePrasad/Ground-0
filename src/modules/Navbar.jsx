373b21eb63f8be898a30e01f5c3010118cfaace8
Prasad Ramdas Hiwarkhede <143614636+HiwarkhedePrasad@users.noreply.github.com>
2024-10-08 14:38:33 +0530

    Update Navbar.jsx

--- src/modules/Navbar.jsx
+++ src/modules/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import LoginButton from './LoginButton';
-import SignupButton from './SignupButton';
+import LoginBtn  from './loginbtn';
+import SignupButton from './Signup';
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center p-4 bg-gray-800 text-white">
       <h1 className="text-2xl font-bold">MyApp</h1>
       <div className="flex space-x-4">
-        <LoginButton />
+        <LoginBtn  />
         <SignupButton />
       </div>
     </nav>
