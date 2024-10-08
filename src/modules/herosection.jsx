9979e3123d245459928e1601948516f076d565fd
Prasad Ramdas Hiwarkhede <143614636+HiwarkhedePrasad@users.noreply.github.com>
2024-10-08 15:34:30 +0530

    Update herosection.jsx

--- src/modules/herosection.jsx
+++ src/modules/herosection.jsx
@@ -1,31 +1,26 @@
 import Spline from '@splinetool/react-spline';
 
-
-
 const HeroSection = () => {
   return (
-    <>
-      <div class=" h-screen flex items-center justify-center">
-        <div class="text-center text-white">
-          <h1 class="text-5xl font-bold mb-4">
-            Connect with Fellow Developers
-          </h1>
-          <p class="text-lg mb-6">
-            Easily find and collaborate with developers who share your
-            interests.
-          </p>
-           <Spline
-        scene="https://prod.spline.design/PY531-6KKJb8AM0B/scene.splinecode" 
-      />
-          <a
-            href="/search"
-            class="bg-white text-blue-500 hover:bg-blue-100 px-6 py-3 rounded-lg font-semibold transition duration-300"
-          >
-            Join Now
-          </a>
-        </div>
+    <div className="h-screen flex items-center justify-center">
+      <div className="text-center text-white">
+        <h1 className="text-5xl font-bold mb-4">
+          Connect with Fellow Developers
+        </h1>
+        <p className="text-lg mb-6">
+          Easily find and collaborate with developers who share your interests.
+        </p>
+        <Spline
+          scene="https://prod.spline.design/PY531-6KKJb8AM0B/scene.splinecode" 
+        />
+        <a
+          href="/search"
+          className="bg-white text-blue-500 hover:bg-blue-100 px-6 py-3 rounded-lg font-semibold transition duration-300"
+        >
+          Join Now
+        </a>
       </div>
-    </>
+    </div>
   );
 };
 
