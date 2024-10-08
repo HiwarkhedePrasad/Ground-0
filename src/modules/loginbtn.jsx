8f9f889d83d642fc86d52d474bdc3fd4ebe6e25a
Prasad Ramdas Hiwarkhede <143614636+HiwarkhedePrasad@users.noreply.github.com>
2024-10-08 14:07:45 +0530

    Update loginbtn.jsx

--- src/modules/loginbtn.jsx
+++ src/modules/loginbtn.jsx
@@ -1,8 +1,8 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginBtn = () => {
-  const { loginWithRedirect } = useAuth0();
-
+  const { loginWithRedirect , user } = useAuth0();
+  console.log(user);
   return (
     <button
       className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
