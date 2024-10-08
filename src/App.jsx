ddfc155e62446def81091a0d8689a29480262eff
Prasad Ramdas Hiwarkhede <143614636+HiwarkhedePrasad@users.noreply.github.com>
2024-10-08 14:37:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homePgae"; // Make sure the file name is correct
 import SearchBox from "./modules/searchbox";
 import ProfilePage from "./pages/profilepage";
+import Navbar from "../modules/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/search" element={<SearchBox />} />
