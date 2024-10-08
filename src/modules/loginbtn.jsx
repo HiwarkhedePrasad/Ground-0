d34a0a2f1ce222fcc98afe392f69125c8aa10d76
Prasad Ramdas Hiwarkhede <143614636+HiwarkhedePrasad@users.noreply.github.com>
2024-10-08 14:29:28 +0530

    Update loginbtn.jsx

--- src/modules/loginbtn.jsx
+++ src/modules/loginbtn.jsx
@@ -1,19 +1,29 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginBtn = () => {
-  const { loginWithRedirect, user, isAuthenticated ,logout} = useAuth0();
+  const { loginWithRedirect, user, isAuthenticated, logout } = useAuth0();
 
   const handleLogin = async () => {
     await loginWithRedirect();
   };
+
+  // Log user info to the console if authenticated
+  if (isAuthenticated && user) {
+    console.log("User Info:", user);
+  }
 
   return (
     <div>
       {isAuthenticated && user ? (
         <div>
           <h2>Welcome, {user.name}!</h2>
-          <pre>{JSON.stringify(user, null, 2)}</pre>
-          <button onclick={e=>logout} className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50" > Lo0gout</button>
+         
+          <button 
+            onClick={logout} 
+            className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+          >
+            Logout
+          </button>
         </div>
       ) : (
         <button
