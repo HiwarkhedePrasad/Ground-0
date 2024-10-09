11d50b186996552e2941dab5f11862fa99c332f0
Prasad Ramdas Hiwarkhede <143614636+HiwarkhedePrasad@users.noreply.github.com>
2024-10-09 14:28:25 +0000

    updated the navbar

--- src/modules/Navbar.jsx
+++ src/modules/Navbar.jsx
@@ -4,7 +4,7 @@ import SignupButton from './Signup';
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center p-4 bg-gray-800 text-white">
+    <nav className="flex justify-between items-center p-4  text-white">
       <h1 className="text-2xl font-bold">MyApp</h1>
       <div className="flex space-x-4">
         <LoginBtn  />
