601de68af7608f45fcbb88974a3e7e9b9220e361
Prasad Ramdas Hiwarkhede <143614636+HiwarkhedePrasad@users.noreply.github.com>
2024-11-21 06:12:59 +0000

    noting much

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: './src', // Your source code directory
+  root: '.', // Your source code directory
   build: {
     outDir: '../dist', // The output directory for the build
   },
