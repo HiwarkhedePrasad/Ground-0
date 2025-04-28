19ab1a4a85f75da4dc247c99e234906d77e5136e
Prasad Ramdas Hiwarkhede <143614636+HiwarkhedePrasad@users.noreply.github.com>
2025-04-28 08:52:57 +0000

    Cange the service github.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/navbar";
 import { SearchByLocation } from "./components/SearchByLocation";
 import Home from "./components/home";
+import Profile from "./components/Profile";  // Make sure to import your Profile component
+
 function App() {
   return (
     <Router>
-      
-      <Navbar/>
+      <Navbar />
 
-        {/* Main Content */}
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/search-location" element={<SearchByLocation />} />
+      {/* Main Content */}
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search-location" element={<SearchByLocation />} />
           <Route path="/profile/:userId" element={<Profile />} /> 
-          </Routes>
-        </main>
-   
+        </Routes>
+      </main>
     </Router>
   );
 }
+
 export default App;
